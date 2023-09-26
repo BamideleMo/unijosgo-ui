ebb05be0ce5fa74d520bbe54b9f81315f0e363ef
Bamidele Moses <bamidele@Bamideles-MacBook-Pro.local>
2023-09-26 08:49:44 +0100

    .

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,13 +1,11 @@
 <script setup></script>
 <template>
-    <RouterLink to="/" class="cursor-pointer drop-shadow-lg flex">
-
-                    <img src="/icon.png" class="h-10 mt-1"/>
+    <RouterLink to="/" class="cursor-pointer drop-shadow-lg">
         <div>
             <span class="text-3xl font-extrabold hover:opacity-60">
                 <span class="text-black">Kampa</span>
             </span>
-            <div class="-mt-1.5 text-gray-600 text-xs">
+            <div class="-mt-1 text-gray-600 text-xs">
                 <span class="hover:opacity-60 myred">www.kampa.ng</span>
             </div>
         </div>

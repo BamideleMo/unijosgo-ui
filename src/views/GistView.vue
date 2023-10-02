10211501d6e1c5b2a89fd10f7fde02fb237330c1
Bamidele Moses <bamidele@Bamideles-MacBook-Pro.local>
2023-10-03 00:21:31 +0100

    .

--- src/views/GistView.vue
+++ src/views/GistView.vue
@@ -142,7 +142,6 @@ onMounted(async () => {
                     Kampa<b class="text-black uppercase">@{{user.campus}}</b>
                 </span>
             </div>
-            <p>Hello, 👋🏾</p>
             <div v-html="volumeContent.gist"></div>
         </div>
     </div>

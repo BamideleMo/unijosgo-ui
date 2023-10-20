c338569710a26a16c32dae5b0f6d0f46a7c029a2
BamideleMo <bamidelemo@gmail.com>
2023-10-20 21:52:47 +0100

    .

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="text-black">
     <div class="text-center">
-      <img src="/loading.gif" alt="" class="w-80 mx-auto" />
+      <img src="/loading.gif" alt="" class="w-64 mx-auto" />
       <p class="text-sm text-gray-400 -mt-10 mb-40">Loading.. .</p>
     </div>
   </div>

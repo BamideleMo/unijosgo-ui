6a462a443ce9c7621fa066fc81ab700a4f149559
BamideleMo <bamidelemo@gmail.com>
2023-10-21 04:33:29 +0100

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -191,7 +191,7 @@ useHead({
     color: red;
 }
 .stressed{
-    background-image: linear-gradient(to top, #0095ff, #00bfff, transparent);
+    background-image: linear-gradient(to top, #eeeeee, #00bfff, transparent);
         background-position: 0 85%;
         background-repeat: no-repeat;
         background-size: auto .40rem;
